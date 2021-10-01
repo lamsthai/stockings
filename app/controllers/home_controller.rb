bfb144c8fc812729db38a12a29eff5a75f92d193
Thai Lam <tl019w@att.com>
2021-10-01 13:11:54 -0700

    install devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+  	@api = StockQuote::Stock.new(api_key: 'pk_f58f7394e4424ae7adf6ed4b09d3d6cb')
+
+  	if params[:ticker] 
+  		if params[:ticker] == ""
+  			@nothing = "forget to input the company"
+  		else
+  			begin
+  		       @stock = StockQuote::Stock.quote(params[:ticker])
+  		    rescue =>   error
+  		    
+  		    	@error = params[:ticker] + ": " + error.message
+  		    end
+  		    
+  		end
+  	end
+  end
+
+  def about
   end
 end
